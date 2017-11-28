27eba38b54a7a4ed660a3f1391d5adb99f8a8fc8
jayjin <jayjinjay@gmail.com>
2017-11-29 00:17:49 +0900

    [Day1] 스타일 작업, 키보드 입력하면 애니메이션

--- day1-drum/main.js
+++ day1-drum/main.js
@@ -1,5 +1,18 @@
 window.addEventListener('keydown', e => {
     const keyCode = e.keyCode;
     const audioEl = document.querySelector(`audio[data-key="${keyCode}"`);
+    const pianoEl = document.querySelector(`li[data-key="${keyCode}"`);
     audioEl.play();
+    pianoEl.classList.add('playing');
+});
+
+const pianoElList = document.querySelectorAll('li');
+
+pianoElList.forEach(el => {
+    console.log(el);
+    el.addEventListener('transitionend', function(e) {
+        if (e.propertyName === 'transform') {
+            this.classList.remove('playing');
+        }
+    });
 })
