c714381bff2456e195f41e3babfe715d7f9edde1
jayjin <jayjinjay@gmail.com>
2017-11-29 00:39:34 +0900

    [Day1] 마구 눌러도 마구 고양이 나오도록

--- day1-drum/main.js
+++ day1-drum/main.js
@@ -2,17 +2,17 @@ window.addEventListener('keydown', e => {
     const keyCode = e.keyCode;
     const audioEl = document.querySelector(`audio[data-key="${keyCode}"`);
     const pianoEl = document.querySelector(`li[data-key="${keyCode}"`);
+    if(!audioEl) return;
+    audioEl.currentTime = 0;
     audioEl.play();
     pianoEl.classList.add('playing');
 });
 
 const pianoElList = document.querySelectorAll('li');
-
 pianoElList.forEach(el => {
-    console.log(el);
     el.addEventListener('transitionend', function(e) {
         if (e.propertyName === 'transform') {
             this.classList.remove('playing');
         }
     });
-})
+});
